9eb43d052921c7e918c222cdcdf9b2fd005c2773
dnicholson0011 <81931232+dnicholson0011@users.noreply.github.com>
2022-09-14 15:01:12 -0500

    Update LoginTest.java

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -24,7 +24,7 @@ public class LoginTest {
     @BeforeAll
     public static void setUp() {
 
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\d.nicholson\\OneDrive - Accenture\\Desktop\\Test Scripts\\Selenium WebDrivers\\Chrome Drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "add directory here/chromedriver.exe");
 
     }
 
